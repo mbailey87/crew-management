6f964f5fda40b7b071a3f215d7617449fc7fd15a
Matthew Bailey <matthewbailey.webdev@gmail.com>
2024-09-16 11:54:45 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, MatButtonModule, CommonModule, FormsModule],
+  template:'<h1>App Component</h1>',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crew-management';
+
+  ngOnInit(): void {}
+  ngdestroy(): void {}
 }
